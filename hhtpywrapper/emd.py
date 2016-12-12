e613fad3592493fabd582e56a1cf7f43a70192ea
YihaoSu <yhsu@astro.ncu.edu.tw>
2016-12-12 14:56:07 +0800

    add get_oi method to EMD class

--- hhtpywrapper/emd.py
+++ hhtpywrapper/emd.py
@@ -17,3 +17,10 @@ class EMD():
                             run_CEEMD, max_sift, type_spline, modify_BC,
                             rand_type, seed_no, check_signal)
         self.imfs = res['result']
+
+    def get_oi(self):
+        oi = mlab.run_func('ratio1', self.imfs)
+        oi_pair = mlab.run_func('ratioa', self.imfs)
+        oi_dic = {'Non-orthogonal leakage of components': oi['result'],
+                  'Non-orthogonal leakage for pair of adjoining components': oi_pair['result']}
+        return oi_dic
