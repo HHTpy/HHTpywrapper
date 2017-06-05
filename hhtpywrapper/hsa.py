a469029059b77e1843050ae76b7db1a93dad314e
YihaoSu <yhsu@astro.ncu.edu.tw>
2017-06-05 15:07:59 +0800

    add plot_hs method to HSA class

--- hhtpywrapper/hsa.py
+++ hhtpywrapper/hsa.py
@@ -15,5 +15,42 @@ class HSA():
     def __init__(self, imfs, dt, ifmethod='hilbtm', normmethod='pchip', nfilter=0):
         fa_res = mlab.run_func('fa', imfs, dt, ifmethod, normmethod, nfilter, nargout=2)
         fa = fa_res['result']
+        self.imfs = imfs
+        self.ifmethod = ifmethod
+        self.normmethod = normmethod
         self.ifreq = fa[0].T.reshape(-1)
         self.iamp = fa[1].T.reshape(-1)
+
+    def plot_hs(self, time, trange, frange, tres, fres, hsize, sigma,
+                tunit='s', funit='Hz', colorbar = 'energy', savefig_name=''):
+        time = time - time[0]
+        tstart = trange[0]
+        tend = trange[1]
+        fstart = frange[0]
+        fend = frange[1]
+        if colorbar == 'amplitude':
+            runfunc = 'nnspa'
+        elif colorbar == 'energy':
+            runfunc = 'nnspe'
+        res_nnspe = mlab.run_func(runfunc, self.imfs, time[0], time[-1], fres, tres,
+                                  fstart, fend, tstart, tend, self.ifmethod,
+                                  self.normmethod, 0, 0, nargout=3)
+        nt = res_nnspe['result'][0]
+        tscale = res_nnspe['result'][1].reshape(-1)
+        fscale = res_nnspe['result'][2].reshape(-1)
+        res_fspecial = mlab.run_func('fspecial', 'gaussian', hsize, sigma)
+        q = res_fspecial['result']
+        res_filter2 = mlab.run_func('filter2', q, nt)
+        res_nsu = mlab.run_func('filter2', q, res_filter2['result'])
+        nsu = res_nsu['result']
+        fig = plt.figure()
+        ax = plt.axes()
+        hs = ax.pcolormesh(tscale, fscale, nsu ** 0.5)
+        cb = plt.colorbar(hs)
+        cb.set_label('Gaussian smoothing ' + colorbar)
+        ax.set_ylabel('Frequency (' + funit + ')')
+        ax.set_xlabel('Time (' + tunit + ')')
+        if savefig_name == '':
+            plt.show()
+        else:
+            plt.savefig(savefig_name, format='eps', dpi=1000)
